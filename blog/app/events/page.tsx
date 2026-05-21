a24698bc727cdc7b6223ec6780bb686a667e14e4
shelby <53214352+shelbygreen@users.noreply.github.com>
2026-05-21 00:54:45 -0400

    quick banner details change (#7)

--- blog/app/events/page.tsx
+++ blog/app/events/page.tsx
@@ -20,14 +20,14 @@ export default function EventsPage() {
               Campaign Kick-off
             </h1>
             <div className="mb-5 leading-relaxed text-xl">
-              <p>🗓️ Date: Wednesday, May 20th</p>
-              <p className="mb-4">⏰ Time: 6:00pm</p>
+              <p>🗓️ Date: Wednesday, May 28th</p>
+              <p className="mb-4">⏰ Time: 7:00pm</p>
               
-              <p className="mb-4">Join us on Wednesday, May 20th to kick-off the Buy Brightline Campaign!</p>
+              <p className="mb-4">Join us on Wednesday for our weekly campaign call! Drop in to get caught up on our efforts and help us refine our strategy to make Brightline public.</p>
               
               <p className="mb-4">Brightline is on the verge of <a href='https://veronews.com/2026/05/14/substantial-doubt-brightline-can-stay-in-business/'>bankruptcy</a> — and that means Floridians finally have a shot at owning it. No more wealthy shareholders calling the shots. We could have cheaper fares, expanded routes, and a rail system we dream of and deserve.</p>
 
-              <p className="mb-4">But we can't Buy Brightline without a strong grassroots movement that pressures Florida lawmakers to act. Help us build that. Join the kick-off call and help us build the movement before our window of opportunity slams shut.</p>
+              <p className="mb-4">But we can't make Brightline public without a strong grassroots movement that pressures Florida lawmakers to act, before our window of opportunity slams shut.</p>
 
               <p>All are welcome. No campaign experience is needed. We just need people like you who are willing to dream and put in the work to make Brightline public. Bring a friend (or make a new one)!</p>
             </div>
